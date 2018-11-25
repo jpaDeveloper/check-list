7f3f0f4c3f2e943e003039814fa11bc22ad0f647
jpedro12 <joao.sjc1210@gmail.com>
2018-11-25 12:30:56 -0200

    button delete ok

--- src/app/viows/list/list.component.ts
+++ src/app/viows/list/list.component.ts
@@ -7,22 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListComponent implements OnInit {
  itens = [];
- next = 0;
+ next = 0; //tamanho do array
   constructor() { }
 
   ngOnInit() {
   }
 
   addItem(item: string){
-    if(this.itens.length == 0) this.itens[0] = item;
+    if(this.itens.length == 0) this.itens[0] = item; //Caso o array ainda esteja vazio
     else{
     this.next = this.itens.length;
-    this.itens[this.next] = item;
+    this.itens[this.next] = item; //introduzindo o item atual na ultima posicao do array
     }
-    console.log(this.itens);
+    //console.log(this.itens);
   }
 
   deleteItem(index: number, item: string){
     console.log(index, item);
+    this.itens.splice(index, 1);
+    console.log("Array Atual: "+this.itens);
   }
 }
